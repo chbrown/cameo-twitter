09a66d0f5a8126e7c3ed0c53287de7a6e0249e71
Christopher Brown <io@henrian.com>
2014-06-29 13:24:21 -0500

    Refactor connection.install into sqlcmd.

--- bin/commands/install.js
+++ bin/commands/install.js
@@ -1,9 +1,14 @@
 /*jslint node: true */
+var path = require('path');
 var db = require('../../db');
 var logger = require('loge');
 
 module.exports = function(argv) {
-  db.install(function(err) {
+  /** Create the database if it doesn't exist and run schema.sql on it.
+  callback: function(Error | null)
+  */
+  var schema_filepath = path.join(__dirname, '..', '..', 'schema.sql');
+  db.initializeDatabase(schema_filepath, function(err) {
     if (err) {
       logger.error('Database install error: %j. %s', err, err.stack);
     }
--- db.js
+++ db.js
@@ -9,32 +9,3 @@ var connection = module.exports = new sqlcmd.Connection({
   database: database,
 });
 // connection.logger = logger;
-
-connection.install = function(callback) {
-  /** Create the database if it doesn't exist and run schema.sql on it.
-  callback: function(Error | null)
-  */
-  var path = require('path');
-  var schema_filepath = path.join(__dirname, 'schema.sql');
-  connection.databaseExists(function(err, exists) {
-    if (err) return callback(err);
-    logger.info('database "%s" %s', connection.options.database, exists ? 'already exists' : 'does not exist');
-
-    if (!exists) {
-      connection.createDatabase(function(err) {
-        if (err) return callback(err);
-
-        logger.info('created database "%s"', connection.options.database);
-        connection.executeSQLFile(schema_filepath, function(err) {
-          if (err) return callback(err);
-
-          logger.info('executed SQL in "%s"', schema_filepath);
-          callback();
-        });
-      });
-    }
-    else {
-      callback();
-    }
-  });
-};
